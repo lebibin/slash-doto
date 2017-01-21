4353ac00d7a102e727f81ca9a1f129147a0192b3
Kevin Candelaria <kevdashdev@gmail.com>
2017-01-21 20:38:17 +0800

    Add post / implementation that mirrors user command

--- web.rb
+++ web.rb
@@ -2,8 +2,17 @@ require 'sinatra/base'
 
 module SlashDoto
   class Web < Sinatra::Base
+    InvalidTokenError = Class.new(Exception)
     get '/' do
       'El Psy Tuturu?'
+    end
+    post '/' do
+      begin
+        raise(InvalidTokenError) unless params[:token] == ENV['SLACK_TOKEN']
+        params[:text]
+      rescue InvalidTokenError => e
+        "You. are. the. only. exception.."
+      end
     end
   end
 end
