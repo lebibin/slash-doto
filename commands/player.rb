c2fef4d927c8c5e18adedb415991fad985d3de86
Kevin Candelaria <kevdashdev@gmail.com>
2017-01-23 21:12:34 +0800

    Fix offenses for player command class

--- commands/player.rb
+++ commands/player.rb
@@ -15,41 +15,53 @@ module SlashDoto
         url = URI.parse("http://api.opendota.com/api/players/#{@account_id}")
         req = Net::HTTP::Get.new(url.to_s)
         res = Net::HTTP.start(url.host, url.port) do |http|
-          http.request(req)
+          http.request req
         end
-        r = JSON.parse res.body
-        if r.key?('profile')
-          solo_mmr = r['solo_competitive_rank']
-          party_mmr = r['competitive_rank']
-          name = r['profile']['personaname']
-          picture = r['profile']['avatarmedium']
-          url = r['profile']['profileurl']
-          {
-            "response_type": 'in_channel',
-            "text": nil,
-            "attachments": [
-              {
-                "color": '#36a64f',
-                "title": name,
-                "title_link": url,
-                "fields": [
-                  {
-                    "title": 'Solo MMR',
-                    "value": solo_mmr,
-                    "short": true
-                  },                {
-                    "title": 'Party MMR',
-                    "value": party_mmr,
-                    "short": true
-                  }
-                ],
-                "thumb_url": picture
-              }
-            ]
-          }
+        parse_response res.body
+      end
+
+      private
+
+      def parse_response(body)
+        parsed_res = JSON.parse body
+        if parsed_res.key? 'profile'
+          valid_response parsed_res
         else
-          {}
+          invalid_response
         end
+      end
+
+      def valid_response(r)
+        {}.tap do |res|
+          res['response_type'] = 'in_channel'
+          res['thumb_url'] = r['profile']['avatarmedium']
+          res['attachments'] = []
+          res['attachments'][0] = attachment_field(r)
+        end
+      end
+
+      def invalid_response
+        {}
+      end
+
+      def attachment_field(r)
+        {}.tap do |res|
+          res['color'] = '#36a64f'
+          res['title'] = r['profile']['personaname']
+          res['title_link'] = r['profile']['profileurl']
+          res['fields'] = [
+            mmr_field('Solo MMR', r['solo_competitive_rank']),
+            mmr_field('Party MMR', r['competitive_rank'])
+          ]
+        end
+      end
+
+      def mmr_field(title, value)
+        {
+          "title": title,
+          "value": value,
+          "short": true
+        }
       end
     end
   end
